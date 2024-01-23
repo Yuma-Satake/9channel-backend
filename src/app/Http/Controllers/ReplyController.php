880590638c39f1f7f108911162d8c8577a195d46
nasunoyoichi <nasunoyoichi.1030@gmail.com>
2024-01-23 10:45:00 +0900

    getReply と getAllReplies API を追加しました。

--- src/app/Http/Controllers/ReplyController.php
+++ src/app/Http/Controllers/ReplyController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
-    //
+    //repliesテーブルから全てのデータを取得する
+    public function getAllReplies()
+    {
+        return Reply::all();
+    }
+
+    //repliesテーブルからtread_idが一致するものを取得する
+    public function getReply(Request $request)
+    {
+        return Reply::where('thread_id', $request->thread_id)->get();
+    }
 }
