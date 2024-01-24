27c935e10bf9e957b39171ff7745e22a48e6d48f
nasunoyoichi <nasunoyoichi.1030@gmail.com>
2024-01-24 15:11:08 +0900

    サインインのAPIを作成しました。

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * メールアドレスが一致したユーザー情報を取得する
+     * メールアドレスがなかったらステータスコード401を返す
+     * @param Request $request
+     * @return User
+     */
+    public function getUser(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (empty($user)) {
+            return response()->json([
+                'message' => 'user not found'
+            ], 401);
+        }
+        return $user;
+    }
+    
+
 }
