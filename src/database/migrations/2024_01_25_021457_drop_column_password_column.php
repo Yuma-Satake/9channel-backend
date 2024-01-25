1156f9b8f6d1f905b0a03823b8fbf78ee5ae4c6c
nasunoyoichi <nasunoyoichi.1030@gmail.com>
2024-01-25 11:18:52 +0900

    passwordカラムを削除しました。

---
+++ src/database/migrations/2024_01_25_021457_drop_column_password_column.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //passwordカラムを削除
+            $table->dropColumn('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //passwordカラムを追加
+            $table->string('password');
+        });
+    }
+};
